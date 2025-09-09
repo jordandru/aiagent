9ec7c914f248b8c7e270b65c779a50961949d75b
jordandru <jordansboxing@gmail.com>
2025-09-09 13:37:50 -0600

    feat: implement verbose flag for CLI output

--- main.py
+++ main.py
@@ -1,29 +1,43 @@
 import os
+import sys
 from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 
-from google import genai
+
 
 client = genai.Client(api_key=api_key)
 
-import sys
+
 
 def main():
     if len(sys.argv) < 2:
         print("Error: please provide a prompt")
         raise SystemExit(1)
-    
-    prompt = sys.argv[1]
+
+    user_prompt = sys.argv[1]
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]   
+
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
+
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
     print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 if __name__ == "__main__":
     main()
