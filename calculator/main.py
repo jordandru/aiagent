d048784250db4a15b4f20b7388fa05af3810f7f4
jordandru <jordansboxing@gmail.com>
2026-03-18 10:55:00 -0600

    03/18/2026 update #2

--- calculator/main.py
+++ calculator/main.py
@@ -17,25 +17,26 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
-    if len(sys.argv) < 2:
-        print("Error: please provide a prompt")
-        raise SystemExit(1)
 
-    user_prompt = sys.argv[1]
+    user_prompt = "yooo"
 
     messages = [
     types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]   
 
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001",
+        model="gemini-2.0-flash-lite",
         contents=messages,
     )
 
     if "--verbose" in sys.argv:
         print(f"User prompt: {user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    
+    if not response.usage_metadata:
+        raise RuntimeError("Runtime error")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     print(response.text)
 
